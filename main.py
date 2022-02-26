436c7d92722425186549a51918cd5fdebda0444a
Shwetank Chandra <96305925+Uzakun@users.noreply.github.com>
2022-02-26 11:09:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+           #Function
+       #Defining Function           
+#syntax:-
+# def my_function(): #you can write any                         thing u want in place                      of --- my_function 
+#   do this
+#   then do this
+#  .
+#  .
+#  .
+#  .
+#   finally do this
+#but if we run the code it will not work, so to make this code work we have to do something called "calling function"
+#Callling function in this case would be ---- my_function().
+#Example
+# def name():
+#   print("Her name is:- ")
+#   print("Hazel")
+# name()  
+
+             #while loop:-
+#while Something_is_true:
+ #Do Something Repeatedly
+
+
+
+
+
